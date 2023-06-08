bae875b985423c9a9f5dc6d90ff46aab36f526a6
Midiy <midiy009@mail.ru>
2023-06-08 07:06:09 +0300

    Added small demo code

--- PythonCLR/Program.cs
+++ PythonCLR/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace PythonCLR
+﻿using PythonCLR.Core;
+using System.Reflection;
+
+namespace PythonCLR
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string code =
+                """
+                print("abc")
+
+                """;
+
+            PythonTranslator.TranslateCodeToAssembly(code, "Test");
+
+            Assembly createdAssembly = Assembly.LoadFrom("./Test.dll");
+            Type entryPointType = createdAssembly.GetType("Program")!;
+            object instance = Activator.CreateInstance(entryPointType)!;
+            entryPointType.GetMethod("Main")!.Invoke(instance, null);
         }
     }
 }
